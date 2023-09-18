aa45f417892b7fe1f79bff94ec272f0f0fcb7774
L1nkr3rrr <l1nk3rino@gmail.com>
2023-09-18 21:08:51 +0300

    [HILL-004][DEV] - add two new apps Teachers and Groups, make a relation m2o between Teacher with Groups, Student with Group. Make them representable via admin panel. Added representation for models

--- university/Teachers/models.py
+++ university/Teachers/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 
-class Student(models.Model):
+class Teacher(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     age = models.IntegerField()
-    group = models.ForeignKey("Groups.Group", on_delete=models.DO_NOTHING, null=True, related_name="students")
+    group = models.ForeignKey("Groups.Group", on_delete=models.DO_NOTHING, null=True, related_name="teachers")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- university/university/settings.py
+++ university/university/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'students'
+    'students',
+    'Groups',
+    'Teachers',
 ]
 
 MIDDLEWARE = [
